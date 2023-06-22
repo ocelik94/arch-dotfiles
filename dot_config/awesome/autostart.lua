e94d55174ef4427e28a57ec692e925937f0a5f55
Celik, Okan <okan@celik.tech>
2023-06-23 00:49:15 +0200

    fix: revert picom and add picom to autostart of awesomewm

--- dot_config/awesome/autostart.lua
+++ dot_config/awesome/autostart.lua
@@ -11,5 +11,11 @@ awful.spawn.easy_async_with_shell(
     function() awful.spawn("nm-applet --indicator") end
 )
 
+
+awful.spawn.easy_async_with_shell(
+    'ps x | grep "picom" | grep -v grep | awk "{print $1}" | xargs kill',
+    function() awful.spawn("picom") end
+)
+
 -- awful.spawn("xrandr --output HDMI-0 --primary --mode 1280x960 --rate 144")
 -- awful.spawn("xrandr --output HDMI-0 --primary --mode 1920x1080 --rate 144")
